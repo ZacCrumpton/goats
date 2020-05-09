1cc990f3707ae62574823208993408b9b0bddd5c
Zac Crumpton <z.crumpton@gmail.com>
2020-05-09 11:55:08 -0500

    setup good

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>GOATS!!</h2>
+      <button className="btn btn-dark">test</button>
+    </div>
+  );
+}
+
+export default App;
