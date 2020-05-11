783b28de821e330624d5387728953d26690b6388
Zac Crumpton <z.crumpton@gmail.com>
2020-05-11 18:56:32 -0500

    getting goats array in console

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.scss';
+import goatData from '../helpers/data/goatData';
 
-function App() {
-  return (
+class App extends React.Component {
+  // outside render - anything that midfies state
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    console.error('goats', goats);
+  }
+
+  render() {
+    // inside the render - anything we need to do to modify the UI
+    return (
     <div className="App">
       <h2>GOATS!!</h2>
-      <button className="btn btn-dark">test</button>
+     <button className="btn btn-dark">test</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
