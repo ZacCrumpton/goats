4bf8a2ff7b6d4459b4c852a69479b3553b5af440
Zac Crumpton <z.crumpton@gmail.com>
2020-05-11 21:01:21 -0500

    use goat button and event work

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -3,15 +3,25 @@ import React from 'react';
 import './Goat.scss';
 
 class Goat extends React.Component {
+  useGoatEvent = (e) => {
+    const { goat, useAGoat } = this.props;
+    e.preventDefault();
+    useAGoat(goat.id);
+  };
+
   render() {
     const { goat } = this.props;
+
     return (
       <div className="Goat col-3">
-        <div class="card">
-          <img class="card-img-top goatImg" src={goat.imgUrl} alt="Goat Card"/>
-          <div class="card-body">
+        <div className="card">
+          <img className="card-img-top goatImg" src={goat.imgUrl} alt="Goat Card"/>
+          <div className="card-body">
           <h4>{goat.name}</h4>
-          <p class="card-text">Beard Leangth: {goat.beardLength} </p>
+          <p className="card-text">Beard Leangth: {goat.beardLength} </p>
+          </div>
+          <div className="card-footer">
+            <button className="btn btn-dark" onClick={this.useGoatEvent}>Use the Goat</button>
           </div>
         </div>
       </div>
