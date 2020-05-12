735712cad9b1046e435de7acd3c8b98f78f901f7
Zac Crumpton <z.crumpton@gmail.com>
2020-05-11 19:56:51 -0500

    displayed goats

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.scss';
+
 import goatData from '../helpers/data/goatData';
+import GoatCoral from '../components/GoatCoral/GoatCoral';
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
   // outside render - anything that midfies state
 
   componentDidMount() {
     const goats = goatData.getGoats();
-    console.error('goats', goats);
+    // goatBuilder(goats);
+    this.setState({ goats });
   }
 
   render() {
     // inside the render - anything we need to do to modify the UI
+    const { goats } = this.state;
     return (
     <div className="App">
-      <h2>GOATS!!</h2>
-     <button className="btn btn-dark">test</button>
+      <h1>GOAT YOGA LTD</h1>
+      <GoatCoral goats={goats}/>
     </div>
     );
   }
---
+++ src/components/GoatCoral/GoatCoral.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './GoatCoral.scss';
+import Goat from '../Goat/Goat';
+
+class GoatCoral extends React.Component {
+  render() {
+    const { goats } = this.props;
+    console.error('goats?', this.props.goats);
+    const makeGoats = goats.map((goat) => (
+      <Goat goat={goat}/>
+    ));
+
+    return (
+      <div className="GoatCoral d-flex flex-wrap">
+        {makeGoats}
+      </div>
+    );
+  }
+}
+
+export default GoatCoral;
